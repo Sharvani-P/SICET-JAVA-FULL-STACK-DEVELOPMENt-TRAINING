60e1e6ba8363d83f70cdc58cde97b946a4479b51
Sharvani <sharvanipuchaginjala@gmail.com>
2026-01-28 12:54:00 +0530

    Created

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.ptintln("Hello World");
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+
     }
 }
